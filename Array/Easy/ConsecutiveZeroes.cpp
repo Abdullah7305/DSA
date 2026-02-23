9fc73e849d87d2c79d0d4b0ec63ee648d590f447
Abdullah7305 <abdullahsuleman299@gmail.com>
2026-02-23 15:51:54 +0500

    Array Easy problem revision...

---
+++ Array/Easy/ConsecutiveZeroes.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int count = 0;
+        int maxCount = 0;
+        int n = nums.size();
+        int i = 0;
+        while (i < n)
+        {
+
+            if (nums[i] == 1)
+            {
+                ++count;
+                maxCount = max(maxCount, count);
+            }
+            else if (nums[i] == 0)
+            {
+                count = 0;
+            }
+
+            i++;
+        }
+        return maxCount;
+    }
+};
